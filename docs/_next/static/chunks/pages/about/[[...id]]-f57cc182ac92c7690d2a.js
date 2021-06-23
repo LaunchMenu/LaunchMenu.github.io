54047c729e0339005fd69ae26a347e3ddad00023
TarVK <tarvankrieken@gmail.com>
2021-06-23 17:08:16 +0200

    Built the site

--- docs/_next/static/chunks/pages/about/[[...id]]-f57cc182ac92c7690d2a.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[12],{HU5w:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/[[...id]]",function(){return u("LKPS")}])},LKPS:function(n,_,u){"use strict";u.r(_),u.d(_,"__N_SSG",(function(){return o}));var w=u("YuAP"),o=!0;_.default=w.a}},[["HU5w",0,2,1,3,4,5,6,7]]]);
--- src/services/mdx/createStaticMdxPropsRetriever.tsx
+++ src/services/mdx/createStaticMdxPropsRetriever.tsx
@@ -5,7 +5,9 @@ export const createStaticMdxPropsRetriever =
     (dir: string) =>
     async ({params}: {params: {id: string[]}}) => ({
         props: {
-            url: `https://launchmenu.github.io/${dir}/${params.id.join("/")}`,
+            url: `https://launchmenu.github.io/${dir}/${(params.id ?? []).join(
+                "/"
+            )}`,
             ...(await compileMarkdown(dir, params.id)),
             index: await createIndex(dir, params.id),
         },
